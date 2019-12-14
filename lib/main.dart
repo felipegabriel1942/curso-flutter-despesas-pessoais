357b5a611e39a8e79a137a9dc979eeaf395ba02c
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2019-12-14 10:04:22 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var card = Card(
+            color: Colors.red,
+            child: Text('LIST OF TX'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          card
+        ],
+      ),
+    );
+  }
+}
