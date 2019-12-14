09f5ae7f0501e8e1273834bbc38e9baa7de2d4d4
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2019-12-14 11:26:57 -0300

    Adicionando input, criando lista e estilizando widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,17 +16,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'Sapatos novos', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Compras supermercado',
+        amount: 99.99,
+        date: DateTime.now())
+  ];
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    var card = Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'));
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -32,7 +44,65 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          card
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Titulo'),
+                    controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Valor'),
+                    controller: amountController,
+                  ),
+                  FlatButton(
+                    child: Text('Adicionar despesa'),
+                    textColor: Colors.purple,
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    child: Text(
+                      'R\$ ${tx.amount}'.replaceAll('.', ','),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      Text(DateFormat('dd/MM/yyyy').format(tx.date),
+                          style: TextStyle(color: Colors.grey))
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
         ],
       ),
     );
